260c518b845e98b783c63684e9c5372cfc692569
Nick Meharry <nick@nickmeharry.com>
2013-07-13 14:38:31 -0700

    Factor out settings validation

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,9 @@ def _deep_merge(original, update_with):
             result[key] = update_with[key]
     return result
 
+class SettingsError(ValueError):
+    pass
+
 DEFAULT_BASE_SETTINGS = {
     'rules': {
         'picking': 'random',
@@ -31,7 +34,7 @@ DEFAULT_BASE_SETTINGS = {
     },
 }
 
-DEFAULT_HIGHLANDER_SETTINGS = {
+HIGHLANDER_SETTINGS = {
     'rules': {
         'mode': 'highlander',
         'class limits': {
@@ -59,7 +62,7 @@ DEFAULT_HIGHLANDER_SETTINGS = {
     },
 }
 
-DEFAULT_SIXES_SETTINGS = {
+SIXES_SETTINGS = {
     'rules': {
         'mode': 'sixes',
         'class limits': {
@@ -77,6 +80,32 @@ DEFAULT_SIXES_SETTINGS = {
     },
 }
 
+def validate_settings(settings):
+    mode = settings['rules']['mode']
+    if mode == 'highlander':
+        settings['rules']['class limits'] = HIGHLANDER_SETTINGS['rules']['class limits']
+        settings['rules']['valid classes'] = HIGHLANDER_SETTINGS['rules']['valid classes']
+    elif mode == 'sixes':
+        settings['rules']['class limits'] = SIXES_SETTINGS['rules']['class limits']
+        settings['rules']['valid classes'] = SIXES_SETTINGS['rules']['valid classes']
+    elif mode == 'custom':
+        if 'class limits' not in settings['rules']:
+            raise SettingsError('Custom pick mode requires rules -> class limits to be configured')
+        if 'valid classes' not in settings['rules']:
+            settings['rules']['valid classes'] = sorted(settings['rules']['class limits'].keys())
+    else:
+        raise SettingsError('Invalid setting rules -> mode, must be one of "highlander", "sixes", or "custom", not "%s"' % mode)
+
+    picking = settings['rules']['picking']
+    if picking == 'random':
+        pass
+    elif picking == 'captain':
+        raise SettingsError("Captain picking isn't supported yet")
+    else:
+        raise SettingsError('Invalid setting rules -> picking, must be one of "random" or "captain", not "%s"' % picking)
+
+    return settings
+
 def load_settings(filename=None):
     if filename is None:
         filename = path.abspath(path.join(path.dirname(__file__), '..', 'settings.yml'))
@@ -85,13 +114,6 @@ def load_settings(filename=None):
         settings = yaml.load(f)
 
     settings = _deep_merge(DEFAULT_BASE_SETTINGS, settings)
-
-    mode = settings['rules']['mode']
-    if mode == 'highlander':
-        settings = _deep_merge(DEFAULT_HIGHLANDER_SETTINGS, settings)
-    elif mode == 'sixes':
-        settings = _deep_merge(DEFAULT_SIXES_SETTINGS, settings)
-    else:
-        raise ValueError('Invalid setting rules -> mode, must be one of "highlander" or "sixes", not "%s"' % mode)
+    settings = validate_settings(settings)
 
     return settings
